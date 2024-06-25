d36c840dbb9af9e68f6b632b34373788f77db08a
jigyas <jigyassharma7@gmail.com>
2024-06-25 13:44:23 +0530

    after deleting a product rerender the checkout header using mvc

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import {renderPaymentSummary} from './paymentSummary.js';
+import { renderCheckoutHeader } from "./checkoutHeader.js";
 
 export function renderOrderSummary(){
   let cartSummaryHTML='';
@@ -123,9 +124,9 @@ export function renderOrderSummary(){
       link.addEventListener('click', ()=>{
           const productId=link.dataset.productId;
           removeFromCart(productId);
-
+          
+          renderCheckoutHeader();
           renderOrderSummary();
-
           renderPaymentSummary();  //generating alll the html again after clicking delete
 
           updateCartQuantity();
