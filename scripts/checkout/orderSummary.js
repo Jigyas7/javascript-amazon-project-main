ab73c9ade2fcbf0dd4d0b0765c92b7b2198cd13c
jigyas <jigyassharma7@gmail.com>
2024-06-25 12:17:23 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary(){
   let cartSummaryHTML='';
@@ -128,6 +129,8 @@ export function renderOrderSummary(){
           );
           container.remove();
 
+          renderPaymentSummary();  //generating alll the html again after clicking delete
+
           updateCartQuantity();
       });
 
@@ -181,6 +184,7 @@ export function renderOrderSummary(){
             const {productId,deliveryOptionId}=element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();  //generate the new html for the changed delivery option  
           });
       });
     }
