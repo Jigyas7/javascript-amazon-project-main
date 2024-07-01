093c01012c9b8eae23045b283614cce07c780612
jigyas <jigyassharma7@gmail.com>
2024-07-01 16:46:40 +0530

    renaming tests file and put them together

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -13,7 +13,7 @@ describe('test suite:renderOrderSummary',()=>{
          <div class="js-order-summary"></div>
          <div class="js-payment-summary"></div>
          <div class="js-return-to-home-link"></div>
-         <div class="js-checkout-header"></div>
+         <div class="js-checkout-header"></div> 
         `;
         
        
@@ -35,29 +35,7 @@ describe('test suite:renderOrderSummary',()=>{
     })
 
     it('displays the cart',()=>{                          //takes this html and put  it into our test container//
-        document.querySelector('.js-test-container').innerHTML=`
-        <div class="js-order-summary"></div>
-        <div class="js-payment-summary"></div> 
-        <div class="js-return-to-home-link"></div>
-        `;
-        // const productId1="e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-        // const productId2="15b6fc6f-327a-4ec4-896f-486349e85a3d";
-
         
-        //     return JSON.stringify([{
-        //         productId:productId1,
-        //         quantity:2,
-        //         deliveryOptionId:'1'
-        //     },{
-        //         productId:productId2,
-        //         quantity:1,
-        //         deliveryOptionId:'2'
-        //     }]); 
-        //  });
-        //  loadFromStorage();spyOn(localStorage,'getItem').and.callFake(()=>{
-
-        //  renderOrderSummary();
-
          expect(
           document.querySelectorAll('.js-cart-item-container').length
          ).toEqual(2);
